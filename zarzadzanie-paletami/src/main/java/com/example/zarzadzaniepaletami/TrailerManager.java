da47ec039db90039c30b00d2314a43045d710c29
zuzia <zuzia.klap@tlen.pl>
2025-02-04 19:12:40 +0100

    converted to mm

--- zarzadzanie-paletami/src/main/java/com/example/zarzadzaniepaletami/TrailerManager.java
+++ zarzadzanie-paletami/src/main/java/com/example/zarzadzaniepaletami/TrailerManager.java
@@ -8,10 +8,10 @@ public class TrailerManager {
 
     public TrailerManager() {
         trailers = new ArrayList<>();
-        // w centymetrach i kilogramach                                                             objetosc
-        trailers.add(new Trailer("Furgon", 1362, 246, 270,22000)); //90m3
-        trailers.add(new Trailer("Standard", 1362, 245, 267,24000)); //91m3
-        trailers.add(new Trailer("Mega", 1360, 245, 290,24000)); //100m3
+        // w milimetrach i kilogramach                                                             objetosc
+        trailers.add(new Trailer("Furgon", 13620, 2460, 2700,22000)); //90m3
+        trailers.add(new Trailer("Standard", 13620, 2450, 2670,24000)); //91m3
+        trailers.add(new Trailer("Mega", 13600, 2450, 2900,24000)); //100m3
     }
 
     public List<Trailer> getTrailers() {
